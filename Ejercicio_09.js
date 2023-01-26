e0e53306a75b93900ac106d8b9f367e5fff07827
Eleazar Morales Garduño <evil5464@gmail.com>
2023-01-26 11:16:45 -0600

    Declaración de ejecicios como funciones corregido

--- Ejercicio_09.js
+++ Ejercicio_09.js
@@ -1,28 +1,31 @@
 //Dados dos arrays, devolver un array con los elementos comunes entre ambos
 
-let arr1 = [4,6,3,5,7,3,8,9,2,2,4,10];
-let arr2 = [8,9,1,2,1,4,5,10];
-let result=[];
+let arr1 = [4, 6, 3, 5, 7, 3, 8, 9, 2, 2, 4, 10];
+let arr2 = [8, 9, 1, 2, 1, 3, 4, 5, 10];
 
-let mapa = {};
+function comunes(arr1, arr2) {
+    let result = [];
+    let mapa = {};
 
-for (const iterator of arr1) {
-    mapa[iterator] = 0;    
-}
-
-console.log(mapa);
-
-for (const iterator of arr2) {
-    if(mapa[iterator]!=undefined){
-        mapa[iterator]++;
+    for (const iterator of arr1) {
+        mapa[iterator] = 0;
     }
-}
-console.log(mapa);
 
-for (const key in mapa) {
-    if (mapa[key] > 0){
-        result.push(key);
+    // console.log(mapa);
+
+    for (const iterator of arr2) {
+        if (mapa[iterator] != undefined) {
+            mapa[iterator]++;
+        }
     }
+    // console.log(mapa);
+
+    for (const key in mapa) {
+        if (mapa[key] > 0) {
+            result.push(key);
+        }
+    }
+    return result;
 }
 
-console.log(result);
+console.log(comunes(arr1, arr2));
