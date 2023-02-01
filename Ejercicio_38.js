d7a0b39733ed79a3dd8854cc4bccb877e0af82e2
Eleazar Morales Garduño <evil5464@gmail.com>
2023-02-01 11:24:13 -0600

    Ejercicio_38 completado

---
+++ Ejercicio_38.js
@@ -0,0 +1,9 @@
+/* Dado un número mostrar el cuadrado de todos los números hasta llegar al mismo */
+
+function cuadrados(n){
+    for (let i = 0; i < n; i++) {
+        console.log(i*i);
+    }
+}
+
+cuadrados(10);
